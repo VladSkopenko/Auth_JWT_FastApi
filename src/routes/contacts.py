d809c5126aa588c56465b62713f661be7d6d0033
VladSkopenko <skopirka2k17@gmail.com>
2024-03-30 16:09:35 +0300

    create funch for admin and moder

--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -2,12 +2,37 @@ from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db import get_db
-from src.database.models import User
+from src.database.models import User, Role
 from src.repository import contacts as repository_contact
 from src.schemas.contact import ContactSchema, ContactUpdateSchema, ContactResponse, ContactStatusUpdate
 from src.services.auth import auth_service
+from src.services.role import RoleAccess
 
 router = APIRouter(prefix='/contacts', tags=['contacts'])
+access_to_route_all = RoleAccess([Role.admin, Role.moderator])
+
+
+@router.get('/', response_model=list[ContactResponse])
+async def get_contacts(limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
+                       db: AsyncSession = Depends(get_db),
+                       current_user: User = Depends(auth_service.get_current_user)):
+    contacts = await repository_contact.get_contacts(limit, offset, db, current_user)
+    return contacts
+
+
+@router.get('/all', response_model=list[ContactResponse], dependencies=[Depends(access_to_route_all)])
+async def get_all_contacts(limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
+                       db: AsyncSession = Depends(get_db),
+                       current_user: User = Depends(auth_service.get_current_user)):
+    contacts = await repository_contact.get_contacts(limit, offset, db)
+    return contacts
+
+
+@router.post('/', response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
+async def create_contact(body: ContactSchema, db: AsyncSession = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repository_contact.create_contact(body, db, current_user)
+    return contact
 
 
 @router.get('/search/', response_model=list[ContactResponse])
@@ -24,27 +49,12 @@ async def get_birthday_contacts(days: int = Query(7, ge=1), db: AsyncSession = D
     return contacts
 
 
-@router.get('/', response_model=list[ContactResponse])
-async def get_contacts(limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
-                       db: AsyncSession = Depends(get_db),
-                       current_user: User = Depends(auth_service.get_current_user)):
-    contacts = await repository_contact.get_contacts(limit, offset, db, current_user)
-    return contacts
-
-
 @router.get('/{contact_id}', response_model=ContactResponse)
 async def get_contact(contact_id: int = Path(ge=1), db: AsyncSession = Depends(get_db),
                       current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contact.get_contact(contact_id, db, current_user)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NOT FOUND")
-    return contact
-
-
-@router.post('/', response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
-async def create_contact(body: ContactSchema, db: AsyncSession = Depends(get_db),
-                         current_user: User = Depends(auth_service.get_current_user)):
-    contact = await repository_contact.create_contact(body, db, current_user)
     return contact
 
 
@@ -71,5 +81,3 @@ async def update_status_contact(body: ContactStatusUpdate, contact_id: int, db: 
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
     return contact
-
-
