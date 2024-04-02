c7e35955b0f02bb132edb60d0f516064b1ef8fae
VladSkopenko <skopirka2k17@gmail.com>
2024-04-02 17:57:45 +0300

    make open cheacker

--- src/routes/check_open.py
+++ src/routes/check_open.py
@@ -10,5 +10,7 @@ router = APIRouter(prefix='/check_open', tags=['check_open'])
 
 @router.get("/{username}")
 async def check_open_f(username: str, response: Response, db: AsyncSession = Depends(get_db)):
-    print(f"{username} зберігаємо що він відкрив email в бд")
+    print("-" * 100)
+    print(f"{username} відкрив email")
+    print("-" * 100)
     return FileResponse("src/static/open_check.png", media_type="image.png", content_disposition_type="inline")
