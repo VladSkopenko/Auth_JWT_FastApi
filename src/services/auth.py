ca076c04cff578cdfe08d810ed955119724fec33
VladSkopenko <skopirka2k17@gmail.com>
2024-04-02 16:47:14 +0300

    get email from token added

--- src/services/auth.py
+++ src/services/auth.py
@@ -84,5 +84,15 @@ class Auth:
         token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return token
 
+    async def get_email_from_token(self, token: str):
+        try:
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            email = payload["sub"]
+            return email
+        except JWTError as e:
+            print(e)
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                                detail="Invalid token for email verification")
+
 
 auth_service = Auth()
