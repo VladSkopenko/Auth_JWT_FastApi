401f01775fb2eb51ea84720ae3b86deeb7592d8d
VladSkopenko <skopirka2k17@gmail.com>
2024-04-04 14:30:39 +0300

    I added cvhange avatar with cloudname

--- src/routes/users.py
+++ src/routes/users.py
@@ -5,10 +5,11 @@ from fastapi import File
 from fastapi import UploadFile
 from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.ext.asyncio import AsyncSession
-
+import pickle
 from src.database.config import config
 from src.database.db import get_db
 from src.database.models import User
+from src.repository import users as repository_users
 from src.schemas.user import UserResponse
 from src.services.auth import auth_service
 
@@ -35,9 +36,17 @@ async def get_current_user(user: User = Depends(auth_service.get_current_user)):
     response_model=UserResponse,
     dependencies=[Depends(RateLimiter(times=2, seconds=5))],
 )
-async def get_current_user(
+async def update_avatar(
     file: UploadFile = File(),
     user: User = Depends(auth_service.get_current_user),
     db: AsyncSession = Depends(get_db),
 ):
+    public_id = f"PyCourse/{user.email}"
+    res = cloudinary.uploader.upload(file.file, public_id=public_id, owerite=True)
+    res_url = cloudinary.CloudinaryImage(public_id).build_url(
+        width=250, height=250, crop="fill", version=res.get("version")
+    )
+    user = await repository_users.update_avatar_url(user.email, res_url, db)
+    # auth_service.cache.set(user.email, pickle.dumps(user))
+    # auth_service.cache.expire(user.email, 300)
     return user
