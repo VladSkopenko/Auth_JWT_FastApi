ed1235422df80eb83b381a008fd54d329b61bbed
VladSkopenko <skopirka2k17@gmail.com>
2024-04-12 18:47:04 +0300

    create test_login

--- tests/test_e2e_auth.py
+++ tests/test_e2e_auth.py
@@ -4,7 +4,9 @@ from main import app
 from datetime import datetime
 import pytest
 from fastapi import HTTPException, status
-
+from conftest import TestingSessionLocal
+from sqlalchemy import select
+from src.database.models import User
 
 user_data = {
     "username": "testauth",
@@ -37,3 +39,55 @@ def test_repeat_signup(client, monkeypatch):
     assert response.status_code == 409, response.text
     data = response.json()
     assert data["detail"] == messages.ACCOUNT_EXIST
+
+
+def test_not_user_login(client):
+    response = client.post(
+        "api/auth/login",
+        data={
+            "username": "Invalid email",
+            "password": "nothing",
+        },
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == messages.INVALID_EMAIL
+
+
+def test_not_confirmed_login(client):
+
+    response = client.post(
+        "api/auth/login",
+        data={
+            "username": user_data.get("email"),
+            "password": user_data.get("password"),
+        },
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == messages.EMAIL_NOT_CONFIRMED
+
+
+@pytest.mark.asyncio
+async def test_login(client):
+    async with TestingSessionLocal() as session:
+        current_user = await session.execute(
+            select(User).where(User.email == user_data.get("email"))
+        )
+        current_user = current_user.scalar_one_or_none()
+        if current_user:
+            current_user.confirmed = True
+            await session.commit()
+
+    response = client.post(
+        "api/auth/login",
+        data={
+            "username": user_data.get("email"),
+            "password": user_data.get("password"),
+        },
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "access_token" in data
+    assert "refresh_token" in data
+    assert "token_type" in data
