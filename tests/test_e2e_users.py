7b7cb4ba1100c49c3c63c9256702a94bc473b6d9
VladSkopenko <skopirka2k17@gmail.com>
2024-04-13 16:08:02 +0300

    finish test

--- tests/test_e2e_users.py
+++ tests/test_e2e_users.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch, MagicMock, AsyncMock
-from src.database.models import User
+from unittest.mock import AsyncMock
+from unittest.mock import patch
+
 from main import app
 from src.services.auth import auth_service
 
@@ -7,7 +8,18 @@ app.user_middleware = []
 
 
 def test__current_user(client, get_token, monkeypatch):
+    """
+    The test__current_user function tests the /api/users/me endpoint.
+    It does this by first patching the auth_service's cache with a mock object, and then setting up mocks for FastAPILimiter's redis, identifier, and http_callback functions.
+    Then it makes a GET request to /api/users/me with an Authorization header containing a valid JWT token (obtained from get_token).
+    Finally it asserts that the response status code is 200.
 
+    :param client: Make requests to the api
+    :param get_token: Get a token for the user
+    :param monkeypatch: Replace the redis and identifier functions with asyncmock
+    :return: The current user
+    :doc-author: Trelent
+    """
     with patch.object(auth_service, "cache") as redis_mock:
         redis_mock.get.return_value = None
 
@@ -21,4 +33,3 @@ def test__current_user(client, get_token, monkeypatch):
             headers=headers,
         )
         assert response.status_code == 200, response.text
-
